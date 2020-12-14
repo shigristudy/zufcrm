de18ff864c04223d7d54eda93d7f872728e7b5db
Kabeer <kabeer@pop-os.localdomain>
2020-12-14 20:56:43 +0500

    .

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -125,6 +125,6 @@ table.dataTable thead .sorting:before, table.dataTable thead .sorting_asc:before
   <script src="{{ asset('app-assets/js/core/app-menu.js') }}"></script>
   <script src="{{ asset('app-assets/js/core/app.js') }}"></script>
   {{-- Load the application scripts --}}
-  <script src="{{ mix('dist/js/app.js') }}"></script>
+  <script src="{{ assets('dist/js/app.js') }}"></script>
 </body>
 </html>
